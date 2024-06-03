33fef393be14672f5ad21bfcf15a86c339fe48ff
Acutapugione <acuta.pugione@gmail.com>
2024-06-03 21:47:52 +0300

    added botcommands

--- bot.py
+++ bot.py
@@ -8,7 +8,12 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from commands import FILMS_COMMAND
+from commands import (
+    FILMS_COMMAND, 
+    START_COMMAND, 
+    FILMS_BOT_COMMAND,
+    START_BOT_COMMAND,
+)
 from data import get_films
 from keyboards import films_keyboard_markup
 
@@ -33,11 +38,23 @@ async def films(message: Message) -> None:
         reply_markup=markup
     )
 
+# @dp.callback_query(StartCallback.filter())
+# async def callb_start(callback: CallbackQuery, callback_data: StartCallback) -> None:
+#     await callback.message.answer(text=f"{callback_data=}")
 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
+    bot = Bot(
+        token=TOKEN, 
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    await bot.set_my_commands(
+        [
+            FILMS_BOT_COMMAND, 
+            START_BOT_COMMAND,
+        ]
+    )
+   
     # And the run events dispatching
     await dp.start_polling(bot)
 
--- commands.py
+++ commands.py
@@ -1,5 +1,9 @@
 # commands.py - модуль в якому оголошені всі необхідні команди(та їх фільтри) 
 from aiogram.filters import Command
-
+from aiogram.types.bot_command import BotCommand
 
 FILMS_COMMAND = Command('films')
+START_COMMAND = Command('start')
+
+FILMS_BOT_COMMAND = BotCommand(command='films', description="Перегляд списку фільмів")
+START_BOT_COMMAND = BotCommand(command='start', description="Почати розмову")
